1a1146127e73918a94db5bc6df011e6f60d89411
Gladson viana <gladsonviana13@gmail.com>
2026-07-09 21:45:25 -0300

    atualizado

---
+++ exercicio22.php
@@ -0,0 +1,44 @@
+<?php
+// entradas - variaveis
+$qtdpaozinho = 100;
+$qtdbroa = 20;
+
+
+//Processamento
+$valorPaozinho = 1.50;
+$valorBroa = 0.12;
+$totalArrecadado = $valorPaozinho + $valorBroa;
+$poupanca = $totalArrecadado * 0.10
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+
+<body>
+
+    <h3> Atividade 19</h3>
+
+
+    <p>
+        A padaria Hotpão vende pães franceses e broas todos os dias. Cada pãozinho
+        custa R$ 0,12 e cada broa custa R$ 1,50. Faça um algoritmo que leia a
+        quantidade de pães e broas vendidas, calcule o total arrecadado e quanto deverá
+        ser guardado na poupança, considerando 10% do total.
+        Resultado esperado: com 100 pães e 20 broas vendidas, o total arrecadado será
+        R$ 42,00 e o valor guardado na poupança será R$ 4,20.
+    </p>
+    <p> <?php echo ("Total arrecadado: " . $totalArrecadado);  ?> </p>
+    <p> <?php echo ("Guardar na poupança: " . $poupanca);  ?> </p>
+
+
+
+</body>
+
+</html>
